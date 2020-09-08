07e34c6a87e932e0d0396d0f4799dfc76289d8db
ManateeFan <0219lyf@gmail.com>
2020-09-08 11:15:43 +0800

    Update Tree.hs

--- Tree/Tree.hs
+++ Tree/Tree.hs
@@ -9,12 +9,16 @@ treeInsert:: (Ord a) => a -> Tree a -> Tree a
 treeInsert x EmptyTree = singleton x
 treeInsert x (Node m left right) 
         | x == m = Node m left right
-        | x < m = Node m (insertTree x left) right
-        | x > m = Node m left (insertTree x right)
+        | x < m  = Node m (treeInsert x left) right
+        | otherwise  = Node m left (treeInsert x right)
 
-treeElem:: (ord a) => a -> Tree a -> Boll
+treeElem:: (Ord a) => a -> Tree a -> Bool
 treeElem x EmptyTree = False
-treeElem x Node m left right 
+treeElem x (Node m left right)
         | x == m = True
         | x > m = treeElem x right
         | x < m = treeElem x left
+
+instance Functor Tree where
+        fmap f EmptyTree = EmptyTree
+        fmap f (Node v left right) = Node (f v) (fmap f left) (fmap f right)
